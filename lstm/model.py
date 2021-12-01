978717f85b4899e80706ff43ce7282fac728fc7f
1paragraph <1paragraph@github.com>
2021-12-01 09:28:55 +0300

    1.4081

--- lstm/model.py
+++ lstm/model.py
@@ -17,11 +17,11 @@ class SiburModel(torch.nn.Module):
         torch.manual_seed(seed)
         super().__init__()
         hidden_size = 128
-        self.lstm = torch.nn.LSTM(11, hidden_size, batch_first=True,
+        self.lstm = torch.nn.LSTM(12, hidden_size, batch_first=True,
                                   num_layers=1, bidirectional=False,
                                   dropout=0.5, bias=False)
 
-        vector_size = 538
+        vector_size = 542
         vector_embedding = 64
         self.cat_linear = torch.nn.Linear(vector_size, vector_embedding, bias=False)
 
